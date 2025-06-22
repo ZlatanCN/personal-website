5fd6d051bd14987d30b941788f4541cf037577f7
ZlatanCN <wy1604010213@163.com>
2025-06-22 23:06:58 +0800

    docs: 初始化项目文档和配置文件
    - 添加 .env.example 文件，包含环境变量示例
    - 创建 .gitattributes 文件，设置文件属性
    - 初始化 .husky、.idea 和 .gitignore 文件
    - 添加 .yarnrc.yml 文件，配置 Yarn
    - 创建博客内容和作者信息文件
    - 添加组件和布局文件
    - 初始化 FAQ 文档
    - 添加 Docker 部署文档
    - 创建数学公式示例文档
    - 添加自定义 MDX 组件文档
    - 初始化 KBar 搜索配置文档

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  {
+    ignores: [],
+  },
+  js.configs.recommended,
+  ...compat.extends(
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.amd,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'commonjs',
+
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-curly-brace-presence': [
+        'error',
+        {
+          props: 'always',
+          children: 'never',
+        },
+      ],
+    },
+  },
+];
