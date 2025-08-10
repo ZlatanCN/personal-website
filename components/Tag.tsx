189fc0b6c4b3959a0490cc8f14e7636400411742
ZlatanCN <wy1604010213@163.com>
2025-08-10 21:29:43 +0800

    refactor: update tag data and improve Tag component styling

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -10,7 +10,7 @@ const Tag = ({ text }: Props) => {
     <Link
       href={`/tags/${slug(text)}`}
       className={
-        'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase'
+        'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium'
       }
     >
       {text.split(' ').join('-')}
